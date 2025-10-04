1a0d1228be271e7f05919ff77086f5c13ddf7fe1
Ali Shan <108082210+alishan4@users.noreply.github.com>
2025-10-04 11:23:46 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  // Optional: Ensure base path works for Vercel
+  base: './',  // Use relative paths for assets
 });
-
-
-
